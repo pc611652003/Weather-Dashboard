603d77dfbf571e6bd90032f43448d54a2f370c12
pc611652003 <88979797+pc611652003@users.noreply.github.com>
2021-10-24 23:06:10 -0600

    added fetch functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,17 +5,35 @@ var clickClearBtn = document.getElementById("clearBtn");
 
 var currentResult = {
     city: "",
+    icon: "",
     temp: "",
     wind: "",
     humidity: "",
-    uni: ""
+    uvi: ""
 }
 var forecastResult = [
-    {temp: "", wind: "", humidity: ""},
-    {temp: "", wind: "", humidity: ""},
-    {temp: "", wind: "", humidity: ""},
-    {temp: "", wind: "", humidity: ""},
-    {temp: "", wind: "", humidity: ""}
+    {icon: "", temp: "", wind: "", humidity: ""},
+    {icon: "", temp: "", wind: "", humidity: ""},
+    {icon: "", temp: "", wind: "", humidity: ""},
+    {icon: "", temp: "", wind: "", humidity: ""},
+    {icon: "", temp: "", wind: "", humidity: ""}
+]
+
+// dummy for testing
+var dummycurrentResult = {
+    city: "salt lake city",
+    icon: "04n",
+    temp: "57.27",
+    wind: "26.46",
+    humidity: "64",
+    uvi: "0"
+}
+var dummyforecastResult = [
+    {icon: "10d", temp: "59.54", wind: "26.35", humidity: "50"},
+    {icon: "10d", temp: "66.33", wind: "47.76", humidity: "37"},
+    {icon: "10d", temp: "45.19", wind: "24.58", humidity: "66"},
+    {icon: "10d", temp: "48.94", wind: "13.69", humidity: "60"},
+    {icon: "04d", temp: "54.07", wind: "8.46", humidity: "56"}
 ]
 
 
@@ -50,6 +68,34 @@ var clearSearchHistory = function () {
 
 var searchWeather = function (lat, lon) {
     console.log("Searching Local Weather ...");
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat="
+    + lat + "&lon=" + lon + "&exclude=hourly,minutely&units=imperial&appid=7888b0b3f54eed5b3244e18d1cfc2e1d";
+
+    fetch(apiUrl).then(function(response) {
+        // request was successful
+        if (response.ok) {
+          response.json().then(function(data) {
+            console.log("Waiting");
+            console.log(data);
+            // pass response data to dom function
+            currentResult.icon = data.current.weather[0].icon;
+            currentResult.temp = data.current.temp;
+            currentResult.wind = data.current.wind_gust;
+            currentResult.humidity = data.current.humidity;
+            currentResult.uvi = data.current.uvi;
+
+            for (var i = 0; i < 5; i++) {
+                forecastResult[i].icon = data.daily[i].weather[0].icon;
+                forecastResult[i].temp = data.daily[i].temp.day;
+                forecastResult[i].wind = data.daily[i].wind_gust;
+                forecastResult[i].humidity = data.daily[i].humidity;
+            }
+          });
+        }
+        else {
+            console.log("Open Weather Map is having trouble.");
+        }
+    });
     displayWeather();
 }
 
@@ -60,51 +106,54 @@ var searchCity = function (targetCity) {
         // request was successful
         if (response.ok) {
           response.json().then(function(data) {
-            // pass response data to dom function
+            // Get latitude and longitude to search weather
             var lat = data.data[0].latitude;
             var lon = data.data[0].longitude;
             searchWeather(lat, lon);
           });
         }
         else {
-            console.log("You are in trouble!");
+            console.log("Position Stack is having trouble.");
         }
     });
 }
 
-submitSearchBtn.addEventListener("click", function(event) {
+var masterSearch = function (event) {
     event.preventDefault();
 
     var searchHistory = localStorage.getItem("searchHistory");
     currentResult.city = searchInputValue.value;
-    
-    if (!searchHistory) {
-        var temp = [];
-        temp.push(searchInputValue.value);
-        localStorage.setItem("searchHistory", JSON.stringify(temp));
-    } else {
-        var searchHistoryRecord = JSON.parse(searchHistory);
-        if (searchHistoryRecord) {
-            var flag = true;
-            for (var i = 0; i < searchHistoryRecord.length; i++) {
-                if (searchHistoryRecord[i] == searchInputValue.value) {
-                    flag = false;
-                }
-            }
-            if (flag) {
-                searchHistoryRecord.push(searchInputValue.value);
-            }
-            localStorage.setItem("searchHistory", JSON.stringify(searchHistoryRecord));
-        } else {
-            temp = [];
+
+    if (searchInputValue.value) {
+        if (!searchHistory) {
+            var temp = [];
             temp.push(searchInputValue.value);
             localStorage.setItem("searchHistory", JSON.stringify(temp));
+        } else {
+            var searchHistoryRecord = JSON.parse(searchHistory);
+            if (searchHistoryRecord) {
+                var flag = true;
+                for (var i = 0; i < searchHistoryRecord.length; i++) {
+                    if (searchHistoryRecord[i] == searchInputValue.value) {
+                        flag = false;
+                    }
+                }
+                if (flag) {
+                    searchHistoryRecord.push(searchInputValue.value);
+                }
+                localStorage.setItem("searchHistory", JSON.stringify(searchHistoryRecord));
+            } else {
+                temp = [];
+                temp.push(searchInputValue.value);
+                localStorage.setItem("searchHistory", JSON.stringify(temp));
+            }
         }
+        updateButton();
+        searchCity(searchInputValue.value);
     }
-    updateButton();
-    searchCity(searchInputValue.value);
-})
+}
 
+submitSearchBtn.addEventListener("click", masterSearch);
 clickClearBtn.addEventListener("click", clearSearchHistory);
 
 updateButton();
